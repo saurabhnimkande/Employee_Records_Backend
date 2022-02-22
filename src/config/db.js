2e4474ffb420f65999494118af78d51fb4fcd750
SaurabhN16 <91530878+saurabhnimkande@users.noreply.github.com>
2022-02-22 22:42:46 +0530

    Updated the mongo db url

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 module.exports = () => {
-  return mongoose.connect(
-    "mongodb+srv://saurabh:saurabh@cluster0.aw4kn.mongodb.net/employee-database"
-  );
+  return mongoose.connect(process.env.MONGO_URL);
 };
